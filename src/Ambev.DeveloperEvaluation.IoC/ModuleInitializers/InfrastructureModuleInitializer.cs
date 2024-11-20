c21f65e31c1648b3c4bad00376aec89171e0b3d4
Adriano-Severino <adriano.x1@hotmail.com>
2024-11-20 16:58:36 -0300

    add dependencies

--- src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
+++ src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -1,18 +1,51 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+using Ambev.DeveloperEvaluation.Application.Sales.GetAllSales;
+using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+using Ambev.DeveloperEvaluation.Application.Sales.UpdateSale;
+using Ambev.DeveloperEvaluation.Domain.Events;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.Infrastructure.Handlers;
 using Ambev.DeveloperEvaluation.ORM;
 using Ambev.DeveloperEvaluation.ORM.Repositories;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales;
+using FluentValidation;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
-
-public class InfrastructureModuleInitializer : IModuleInitializer
+namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers
 {
-    public void Initialize(WebApplicationBuilder builder)
+    public class InfrastructureModuleInitializer : IModuleInitializer
     {
-        builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        public void Initialize(WebApplicationBuilder builder)
+        {
+            // Registro dos repositórios
+            builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
+            builder.Services.AddTransient<IUserRepository, UserRepository>();
+            builder.Services.AddTransient<ISaleRepository, SaleRepository>();
+
+            // Registro dos handlers de eventos
+            builder.Services.AddTransient<INotificationHandler<SaleCreated>, SaleEventHandler>();
+            builder.Services.AddTransient<INotificationHandler<SaleModified>, SaleEventHandler>();
+            builder.Services.AddTransient<INotificationHandler<SaleCancelled>, SaleEventHandler>();
+            builder.Services.AddTransient<INotificationHandler<ItemCancelled>, SaleEventHandler>();
+
+            // Registro dos validadores
+            //builder.Services.AddTransient<IValidator<CreateSaleRequest>, CreateSaleRequestValidator>();
+            builder.Services.AddTransient<IValidator<GetSaleRequest>, GetSaleRequestValidator>();
+            builder.Services.AddTransient<IValidator<UpdateSaleRequest>, UpdateSaleRequestValidator>();
+            //builder.Services.AddTransient<IValidator<DeleteSaleRequest>, DeleteSaleRequestValidator>();
+            builder.Services.AddTransient<IValidator<DeleteSaleCommand>, DeleteSaleCommandValidator>();
+
+            // Registro dos handlers
+            builder.Services.AddTransient<IRequestHandler<CreateSaleCommand, CreateSaleResult>, CreateSaleHandler>();
+            builder.Services.AddTransient<IRequestHandler<GetAllSalesCommand, List<GetAllSalesResult>>, GetAllSalesHandler>();
+            builder.Services.AddTransient<IRequestHandler<GetSaleCommand, GetSaleResult>, GetSaleHandler>();
+            builder.Services.AddTransient<IRequestHandler<UpdateSaleCommand, UpdateSaleResult>, UpdateSaleHandler>();
+            builder.Services.AddTransient<IRequestHandler<DeleteSaleCommand, Unit>, DeleteSaleHandler>();
+
+        }
     }
 }
